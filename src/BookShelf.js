ec5ffea84ff27abea1a956a5e1a9a43b5ce66916
zyud <zyud@seas.upenn.edu>
2018-06-18 12:40:42 -0400

    refactor: Change BookShelf from regular component to stateless functional component

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -3,35 +3,33 @@ import PropTypes from 'prop-types'
 import './App.css'
 import Book from './Book'
 
-export default class BookShelf extends React.Component {
-    render() {
-        const { books, heading, onOptionChange } = this.props
+const BookShelf = (props) => {
+    const { books, heading, onOptionChange } = props
 
-        return (
-            <div>
-                <div className='shelf-heading'>
-                    <h3>{heading}</h3>
-                </div>
-                <hr/>
-                <div className='shelf'>
-                    {books && Object.keys(books).length > 0
-                    &&   <div className='books-container'>
-                            {Object.keys(books).map(key => (
-                                <Book
-                                    key={books[key].id}
-                                    id={books[key].id}
-                                    authors={books[key].authors}
-                                    title={books[key].title}
-                                    imageLinks={books[key].imageLinks}
-                                    shelf={books[key].shelf}
-                                    onOptionChange={onOptionChange} />
-                            ))}
-                        </div>
-                    }
-                </div>
+    return (
+        <div>
+            <div className='shelf-heading'>
+                <h3>{heading}</h3>
             </div>
-        )
-    }
+            <hr/>
+            <div className='shelf'>
+                {books && Object.keys(books).length > 0
+                &&   <div className='books-container'>
+                        {Object.keys(books).map(key => (
+                            <Book
+                                key={books[key].id}
+                                id={books[key].id}
+                                authors={books[key].authors}
+                                title={books[key].title}
+                                imageLinks={books[key].imageLinks}
+                                shelf={books[key].shelf}
+                                onOptionChange={onOptionChange} />
+                        ))}
+                    </div>
+                }
+            </div>
+        </div>
+    )
 }
 
 BookShelf.propTypes = {
@@ -39,3 +37,5 @@ BookShelf.propTypes = {
     books: PropTypes.arrayOf(PropTypes.object),
     onOptionChange: PropTypes.func.isRequired
 }
+
+export default BookShelf
